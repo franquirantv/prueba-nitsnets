5b930457962bf54f6023a5c45cd0ced1cf20ab3a
Francisco Quirant Vicente <fqv2@gcloud.ua.es>
2023-11-02 16:50:47 +0100

    Fix: gitignore and refactor

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,7 @@
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const sqlite3 = require('sqlite3').verbose();
-
-let sql;
-const db = new sqlite3.Database('./character.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-    console.error(err.message);
-    }
-});
-
 
 const app = express();
 app.use(cors());
